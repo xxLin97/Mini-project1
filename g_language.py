624804efd992ccb83ca9b4bd843d38cc84789cdb
xxLin97 <55032729+xxLin97@users.noreply.github.com>
2019-09-28 08:27:27 +0800

    Update g_language.py

--- g_language.py
+++ g_language.py
@@ -1,9 +1,3 @@
-from google.cloud import language
-from google.cloud.language import enums
-from google.cloud.language import types
-
-#Make a function called get_sentiment_score which takes tweet as the parameter, and returns the sentiment score.
-
 def get_sentiment_score(tweet):
     client = language.LanguageServiceClient()
     document = types\
@@ -14,3 +8,34 @@ def get_sentiment_score(tweet):
                       .document_sentiment\
                       .score
     return sentiment_score
+
+def analyze_tweets(keyword, total_tweets):
+    score = 0
+    tweets = search_tweets(keyword,total_tweets)
+    for tweet in tweets:
+        cleaned_tweet = clean_tweets(tweet.text.encode('utf-8'))
+        sentiment_score = get_sentiment_score(cleaned_tweet)
+        score += sentiment_score
+        print('Tweet: {}'.format(cleaned_tweet))
+        print('Score: {}\n'.format(sentiment_score))
+    final_score = round((score / float(total_tweets)),2)
+    return final_score
+
+def send_score(final_score):
+  if final_score <= -0.25:
+    status = 'NEGATIVE'
+  elif final_score <= 0.25:
+    status = 'NEUTRAL'
+  else:
+    status = 'POSITIVE'
+  print("The atitude of"+str(keyword)+"is"+status)
+
+
+def main():
+  keyword=input("keyword that you want to search")
+  get_sentiment_score(result)
+  analyze_tweets(keyword,50)
+  send_score(final_score)
+    
+if __name__ == '__main__':
+    main()    
